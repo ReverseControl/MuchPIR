0596434ed02de596b2a56426b6c4da98e6dce5de
Escanor Liones <ReverseControl@users.noreply.github.com>
2021-09-06 18:41:16 -0400

    [demo] [sql] Set password for postgres user. Document better.

--- load_data.sql
+++ load_data.sql
@@ -1,3 +1,8 @@
+--Set password for postgres user. 
+--We need this later.
+ALTER USER postgres WITH PASSWORD 'pass';
+
+--Create tables for different size tables
 CREATE TABLE data_10 (                                                             
         nconst            bigint,
         primaryName       text,
@@ -63,7 +68,10 @@ CREATE TABLE data_all (
         knownForTitles    text
 );
 
+--Load the 69420 table from the IMDB dataset sample.
 copy data_all from '/data/sample_imdb.tsv';
+
+--Create smaller tables and store them locally
 COPY (SELECT * FROM data_all ORDER BY nconst ASC LIMIT 1024    ) TO '/tmp/data_10.tsv';
 COPY (SELECT * FROM data_all ORDER BY nconst ASC LIMIT 2048    ) TO '/tmp/data_11.tsv';
 COPY (SELECT * FROM data_all ORDER BY nconst ASC LIMIT 4196    ) TO '/tmp/data_12.tsv';
@@ -72,6 +80,7 @@ COPY (SELECT * FROM data_all ORDER BY nconst ASC LIMIT 16384   ) TO '/tmp/data_1
 COPY (SELECT * FROM data_all ORDER BY nconst ASC LIMIT 32768   ) TO '/tmp/data_15.tsv';
 COPY (SELECT * FROM data_all ORDER BY nconst ASC LIMIT 65536   ) TO '/tmp/data_16.tsv';
 
+--Load table data from tables create above
 copy data_10 from '/tmp/data_10.tsv';
 copy data_11 from '/tmp/data_11.tsv';
 copy data_12 from '/tmp/data_12.tsv';
